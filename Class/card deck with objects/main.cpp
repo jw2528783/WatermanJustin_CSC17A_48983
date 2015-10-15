bd2c5c5e1d3393cbdd46d198f2f90b0a5c4a6574
jw2528783 <jwaterman96@gmail.com>
2015-10-15 12:27:38 -0700

    Signed-off-by: jw2528783 <jwaterman96@gmail.com>

--- Class/card deck with objects/main.cpp
+++ Class/card deck with objects/main.cpp
@@ -3,6 +3,7 @@
  * Author: Justin
  * Created on October 15, 2015, 11:21 AM
  *      Creation of first Object Oriented Program
+ *      Creating a Deck of Cards
  */
 
 #include <cstdlib>
@@ -10,14 +11,11 @@
 #include <ctime>
 using namespace std;
 
-//User Libraries
+//User Library
 #include "card.h"
-//Function Prototypes
 
 int main(int argc, char** argv) {
-    srand(static_cast<unsigned int>(time(0)));
-    //Declare deck suit
-    cout<<"Class Check"<<endl;
+    cout<<endl;
     for(int i=1;i<=52;i++){
         card card(i);
         cout<<"Card Number = "<<static_cast<int>(card.getnum());
@@ -25,6 +23,6 @@ int main(int argc, char** argv) {
         cout<<" Numerical Value = "<<static_cast<int>(card.numval());
         cout<<" Face Value = "<<card.faceval()<<endl;
     }
-    
+    cout<<endl;
     return 0;
 }
