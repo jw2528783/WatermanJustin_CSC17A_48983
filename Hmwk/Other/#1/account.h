882d911b1ef4951122ff0dbf3de3fb6460001331
jw2528783 <jwaterman96@gmail.com>
2015-10-28 17:17:42 -0700

    Signed-off-by: jw2528783 <jwaterman96@gmail.com>

---
+++ Hmwk/Other/#1/account.h
@@ -0,0 +1,19 @@
+/* 
+ * File:   account.h
+ * Author: rcc
+ *
+ * Created on October 28, 2015, 4:38 PM
+ */
+
+#ifndef ACCOUNT_H
+#define	ACCOUNT_H
+
+struct account{
+    int num;
+    int bal;
+    int totch;
+    int totdep;
+};
+
+#endif	/* ACCOUNT_H */
+
---
+++ Hmwk/Other/2/main.cpp
@@ -0,0 +1,14 @@
+/* 
+ * File:   main.cpp
+ * Author: Justin
+ * Created on October 28, 2015, 5:14 PM
+ */
+#include <cstdlib>
+#include <iostream>
+using namespace std;
+
+int main(int argc, char** argv) {
+    
+    return 0;
+}
+
