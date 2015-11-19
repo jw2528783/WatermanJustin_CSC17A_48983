43d6e8cf38687c3b646805c31fb3ce45d87bcce7
jw2528783 <jwaterman96@gmail.com>
2015-11-18 18:05:57 -0800

    Signed-off-by: jw2528783 <jwaterman96@gmail.com>

---
+++ Hmwk/Assignment 5/Gaddis_Ch13_Prob8/main.cpp
@@ -0,0 +1,30 @@
+/* 
+ * File:   main.cpp
+ * Author: Justin
+ * Created on November 18, 2015, 5:30 PM
+ */
+
+#include <iostream>
+using namespace std;
+
+class Circle{
+    private:
+        float radius,pi;
+    public:
+        Circle(){radius=0.0,pi=3.14159;};
+        void setRadius(){cin>>radius;};
+        float getRadius()const{return radius;};
+        float getArea()const{return pi*radius*radius;};
+        float getDiameter()const{return radius*2;};
+        float getCircum()const{return 2*pi*radius;};
+};
+
+int main(int argc, char** argv) {
+    Circle info;
+    cout<<"What is the radius of your circle?"<<endl;
+    info.setRadius();
+    cout<<endl<<"Circle:\n\tRadius: "<<info.getRadius()<<endl;
+    cout<<"\tDiameter: "<<info.getDiameter()<<"\n\tCircumference: "<<info.getCircum()<<endl;
+    cout<<"\tArea: "<<info.getArea()<<endl;
+    return 0;
+}
